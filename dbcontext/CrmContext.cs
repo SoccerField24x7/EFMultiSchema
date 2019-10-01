eec59591a85c8e94b81a18bc9728fb9c0208860b
Jesse Quijano <jesse@quijano.net>
2019-10-01 11:41:39 -0700

    initial commit

---
+++ dbcontext/CrmContext.cs
@@ -0,0 +1,14 @@
+﻿using webtest.dbcontext.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace webtest.dbcontext
+{
+    public class CrmContext : DbContext
+    {
+        public CrmContext(DbContextOptions<CrmContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Name> Names { get; set; }
+    }
+}
---
+++ dbcontext/Models/Name.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webtest.dbcontext.Models
+{
+    public class Name
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public long Id { get; set; }
+
+        [MaxLength(50)]
+        public string UserName { get; set; }
+    }
+}
