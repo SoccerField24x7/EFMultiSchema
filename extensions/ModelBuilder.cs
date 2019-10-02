e1b1807d725c3ac9cdac5a66d9d2aed1add5ea57
Jesse Quijano <jesse@quijano.net>
2019-10-02 16:27:57 -0700

    Replacement for the RC2 RemovePluralizingTableNameConvention

---
+++ extensions/ModelBuilder.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//namespace webtest.extensions;
+
+public static class ModelBuilderExtensions
+{
+    public static void RemovePluralizingTableNameConvention(this ModelBuilder modelBuilder)
+    {
+        foreach (IMutableEntityType entity in modelBuilder.Model.GetEntityTypes())
+        {
+            entity.Relational().TableName = entity.DisplayName();
+        }
+    }
+}
