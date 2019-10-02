3782178c23babff2cd407e4a20f0305f1b5b8a6f
Jesse Quijano <jesse@quijano.net>
2019-10-02 16:31:59 -0700

    EF model for the tenant table

---
+++ dbcontext/Models/Tenant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webtest.dbcontext.Models
+{
+
+    public class Tenant
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        [MaxLength(50)]
+        public string TenantName { get; set; }
+
+        [MaxLength(25)]
+        public string SchemaId { get; set; }
+
+        public byte IsActive { get; set; }
+    }
+}
