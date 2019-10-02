76cd9e89c514989a16f5bf5abc383ec3bad70b8f
Jesse Quijano <jesse@quijano.net>
2019-10-02 14:19:43 -0700

    Add dbcontext creation and schema switching

--- dbcontext/CrmContext.cs
+++ dbcontext/CrmContext.cs
@@ -1,14 +1,81 @@
 ﻿using webtest.dbcontext.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace webtest.dbcontext
 {
     public class CrmContext : DbContext
     {
+        public string SchemaName { get; set; }
+
         public CrmContext(DbContextOptions<CrmContext> options) : base(options)
         {
         }
 
-        public DbSet<Name> Names { get; set; }
+        public CrmContext(string schemaname) : base()
+        {
+            SchemaName = schemaname;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            //var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+            //    .AddJsonFile("appsettings.json");
+            //var configuration = builder.Build();
+
+            var serviceProvider = new ServiceCollection().AddEntityFrameworkSqlServer()
+                .AddSingleton<IModelCustomizer, SchemaContextCustomize>()
+                .BuildServiceProvider();
+
+            optionsBuilder.UseSqlServer(Startup.Configuration.GetConnectionString("DbService")).UseInternalServiceProvider(serviceProvider);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.RemovePluralizingTableNameConvention();
+
+            if (!string.IsNullOrEmpty(SchemaName))
+            {
+                modelBuilder.HasDefaultSchema(SchemaName);
+            }
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public class SchemaContextCustomize : ModelCustomizer
+        {
+            public SchemaContextCustomize(ModelCustomizerDependencies dependencies)
+                : base(dependencies)
+            {
+
+            }
+            public override void Customize(ModelBuilder modelBuilder, DbContext dbContext)
+            {
+                base.Customize(modelBuilder, dbContext);
+
+                string schemaName = (dbContext as CrmContext).SchemaName;
+                (dbContext as CrmContext).SchemaName = schemaName;
+
+            }
+        }
+
+        /* add your table models here */
+        public DbSet<Name> Name { get; set; }
+    }
+}
+
+public static class ModelBuilderExtensions
+{
+    public static void RemovePluralizingTableNameConvention(this ModelBuilder modelBuilder)
+    {
+        foreach (IMutableEntityType entity in modelBuilder.Model.GetEntityTypes())
+        {
+            entity.Relational().TableName = entity.DisplayName();
+        }
     }
 }
